e79077cd2f810dfe78cc47e35c0702e713f3b18c
Alex Gavrisco <alexandr@gavrisco.com>
2019-12-08 22:34:35 +0200

    Implement part2 of day1

--- 2019/src/bin/day1.rs
+++ 2019/src/bin/day1.rs
@@ -1,21 +1,40 @@
 use std::fs::File;
 use std::io::{BufReader, BufRead, Error};
 
+fn calculate_mass_fuel(mass: &i64) -> i64 {
+    let fuel = (*mass as f64 / 3.0).floor() as i64;
+    return fuel - 2;
+}
+
+fn calculate_fuel(mass: &i64) -> i64 {
+    let result = calculate_mass_fuel(mass);
+    if result > 0 {
+        return result + calculate_fuel(&result);
+    } else {
+        return 0;
+    }
+}
+
 fn main() -> Result<(), Error> {
     let path = "data/day1.txt";
 
     let input = File::open(path)?;
     let buffered = BufReader::new(input);
+    let modules_mass: Vec<i64> = buffered.lines()
+        .map(|l| l.unwrap().parse::<i64>().unwrap())
+        .collect();
 
-    let mut result: u32 = 0;
-    for line in buffered.lines() {
-        let value = line.unwrap();
-        let value = value.parse::<u32>().unwrap();
-        let value = (value as f64 / 3.0) as u32;
-        result = result + value - 2;
-    }
+    let result: i64 = modules_mass.iter()
+        .map(|l| calculate_mass_fuel(l))
+        .sum();
 
-    println!("{}", result);
+    println!("Fuel needed for modules: {}", result);
+
+    let result: i64 = modules_mass.iter()
+        .map(|l| calculate_fuel(l))
+        .sum();
+
+    println!("Total fuel: {}", result);
 
     Ok(())
 }
