e7b4aaea262a92296e5eeaddf0a740aa7874ccab
Alex Gavrisco <alexandr@gavrisco.com>
2019-12-29 23:50:51 +0200

    Implement 2nd part of day3

--- 2019/src/bin/day3.rs
+++ 2019/src/bin/day3.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashSet};
 use std::fs::File;
 use std::io::{BufReader, BufRead};
 
@@ -10,7 +10,28 @@ struct Point {
     y: i32,
 }
 
-fn get_wire_coords(wire_path: &String) -> Result<Vec<Point>, Error> {
+fn expand_wire_segment(starting_point: &Point, direction: &str, length: usize) -> Result<(Point, Vec<Point>), Error> {
+    let mut points: Vec<Point> = Vec::with_capacity(length);
+    let mut current_point = Point {
+        x: starting_point.x,
+        y: starting_point.y,
+    };
+    for _ in 0..length {
+        let point = match direction {
+            "L" => Ok(Point { x: current_point.x - 1, y: current_point.y }),
+            "R" => Ok(Point { x: current_point.x + 1, y: current_point.y }),
+            "U" => Ok(Point { x: current_point.x, y: current_point.y + 1 }),
+            "D" => Ok(Point { x: current_point.x, y: current_point.y - 1 }),
+            _ => Err(format_err!("Unknown direction")),
+        }?;
+        current_point.x = point.x;
+        current_point.y = point.y;
+        points.push(point);
+    }
+    Ok((current_point, points))
+}
+
+fn get_wire_path(wire_path: &String) -> Result<Vec<Point>, Error> {
     let mut current_point = Point {
         x: 0,
         y: 0,
@@ -18,21 +39,10 @@ fn get_wire_coords(wire_path: &String) -> Result<Vec<Point>, Error> {
     let mut wire_coords = Vec::new();
     for x in wire_path.trim().split(",") {
         let (direction, length) = x.split_at(1);
-        let length = length.parse::<i32>()?;
-        for _ in 0..length {
-            let point = match direction {
-                "L" => Ok(Point { x: current_point.x - 1, y: current_point.y }),
-                "R" => Ok(Point { x: current_point.x + 1, y: current_point.y }),
-                "U" => Ok(Point { x: current_point.x, y: current_point.y + 1 }),
-                "D" => Ok(Point { x: current_point.x, y: current_point.y - 1 }),
-                _ => Err(format_err!("Unknown direction")),
-            }?;
-            current_point = Point {
-                x: point.x,
-                y: point.y,
-            };
-            wire_coords.push(point);
-        }
+        let length = length.parse::<usize>()?;
+        let wire_segment = expand_wire_segment(&current_point, direction, length)?;
+        current_point = wire_segment.0;
+        wire_coords.extend(wire_segment.1);
     }
     Ok(wire_coords)
 }
@@ -48,17 +58,29 @@ fn main() -> Result<(), Error> {
 
     let mut input = String::new();
     buffered.read_line(&mut input)?;
-    let wire1: HashSet<Point> = get_wire_coords(&input)?.into_iter().collect();
+    let wire1_path = get_wire_path(&input)?;
+    let wire1: HashSet<&Point> = wire1_path.iter().collect();
 
     let mut input = String::new();
     buffered.read_line(&mut input)?;
-    let wire2: HashSet<Point> = get_wire_coords(&input)?.into_iter().collect();
+    let wire2_path = get_wire_path(&input)?;
+    let wire2: HashSet<&Point> = wire2_path.iter().collect();
 
     let origin = Point { x: 0, y: 0};
     let min_distance = wire1.intersection(&wire2)
-        .map(|a| get_distance(&a, &origin))
+        .map(|a| get_distance(a, &origin))
         .min()
         .ok_or(format_err!("Didn't find the min distance. Perhaps there's no intersection."));
-    println!("Min Distance = {}", min_distance?);
+    println!("Intersection with min distance = {}", min_distance?);
+
+    let shortest_path = wire1.intersection(&wire2)
+        .map(|intersection| {
+            let wire1_steps = wire1_path.iter().position(|p| &p == intersection).unwrap();
+            let wire2_steps = wire2_path.iter().position(|p| &p == intersection).unwrap();
+            wire1_steps + wire2_steps + 2
+        })
+        .min()
+        .ok_or(format_err!("Expected to find at least 1 intersection"));
+    println!("Path with to the shortests intersection has {} steps", shortest_path?);
     Ok(())
 }
